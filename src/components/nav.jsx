a66cdcf0defe509abcabb3d1cac3cb32f96540f8
Avinash jha <avinashjha@Avinashs-MacBook-Air.local>
2026-01-18 14:41:04 +0530

    first commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 const nav = () => {
   return (
     <div>
@@ -13,21 +13,21 @@ const nav = () => {
             </div>
             <div className='right'>
               <div className='home'>
-                <p ><a href='/'>Home</a></p>
+                <p ><Link to="/">Home</Link></p>
               </div>
               <div className='skill'>
-                <p><a href='/skill'>Skill</a></p>
+                <p><Link to="/Skill">Skill</Link></p>
               </div>
 
               <div className='edu'>
-                <p><a href='/education'>Education</a></p>
+                <p><Link to="/education">Education</Link></p>
               </div>
 
               <div className='project'>
-                <p><a href='/project'>Project</a></p>
+                <p><Link to="/project">Project</Link></p>
               </div>
               <div className='contact'>
-                <p><a href='/contact'>Contact us</a></p>
+                <p><Link to="/contact">Contact</Link></p>
               </div>
             </div>
           </div>
